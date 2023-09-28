2ac5d91cc90cdbf4776c49251c289b00295ab261
r-tarnowski <radek.github@wp.pl>
2023-09-28 23:37:48 +0200

    Initial commit

---
+++ src/AutoFunctionsVsLambdas.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+#include <vector>
+#include <set>
+#include <ranges>
+#include <map>
+#include <list>
+
+
+using std::cout;
+using std::endl;
+
+void printHeader() {
+    cout << endl;
+    cout << "================================================" << endl;
+    cout << "  C++20 The Complete Guide by Nicolai Josuttis  " << endl;
+    cout << "   Placeholder Types for Functions Parameters   " << endl;
+    cout << "         auto Functions versus Lambdas          " << endl;
+    cout << "================================================" << endl;
+    cout << endl;
+}
+
+
+int main(int argc, char *argv[]) {
+    printHeader();
+    return 0;
+}
+
