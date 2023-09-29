741016b3fbe811560c609c6a288925f5ebe9fa3e
r-tarnowski <radek.github@wp.pl>
2023-09-29 22:48:06 +0200

    Example added

--- src/AutoFunctionsVsLambdas.cpp
+++ src/AutoFunctionsVsLambdas.cpp
@@ -20,9 +20,51 @@ void printHeader() {
     cout << endl;
 }
 
+struct Customer {
+   std::string name;
+   std::string homeTown;
+};
+
+bool lessByNameFunc( const auto & c1, const auto & c2 ) {
+    return c1.name < c2.name;
+}
+
+auto greaterByNameLambda = [] ( const auto & c1, const auto & c2 ) {
+    return c1.name > c2.name;
+};
 
 int main(int argc, char *argv[]) {
-    printHeader();
-    return 0;
+   printHeader();
+
+   std::vector<Customer> customers = { { "Warszawski", "Warszawa"},
+                                       { "Skierniewicki", "Skierniewice" },
+                                       { "Grodziski", "Grodzisk Mazowiecki"},
+                                       { "Opolski", "Opole"},
+                                       { "Kutnowski", "Kutno"} };
+
+   cout << std::endl << "Before sorting:" << std::endl;
+   for ( auto & customer: customers ) {
+       cout << customer.name << ", " << customer.homeTown << std::endl;
+   }
+
+   //std::sort( customers.begin(), customers.end(), lessByNameFunc ); // ERROR! can't deduce type of parameters in sorting criterion
+   std::sort( customers.begin(), customers.end(),
+              lessByNameFunc<Customer, Customer> );
+
+   cout << std::endl << "After sorting with lessByNameFunc criterion:" << std::endl;
+   for ( auto & customer: customers ) {
+       cout << customer.name << ", " << customer.homeTown << std::endl;
+   }
+
+   std::sort( customers.begin(), customers.end(),
+              greaterByNameLambda );
+
+   cout << std::endl << "After sorting with greaterByNameLambda criterion:" << std::endl;
+   for ( auto & customer: customers ) {
+       cout << customer.name << ", " << customer.homeTown << std::endl;
+   }
+
+
+   return 0;
 }
 
